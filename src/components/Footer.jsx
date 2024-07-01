d2722b5bd805e91b05f86796e777e9828b01c2bf
Diana Catalina Olarte <diana.olarte@edunext.co>
2024-07-02 08:59:43 +1000

    fix: update FOOTER_LOGO definition

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { sendTrackEvent } from '@edx/frontend-platform/analytics';
-import { ensureConfig } from '@edx/frontend-platform';
+import { ensureConfig, getConfig } from '@edx/frontend-platform';
 import { AppContext } from '@edx/frontend-platform/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
@@ -20,8 +20,6 @@ ensureConfig([
 const EVENT_NAMES = {
   FOOTER_LINK: 'edx.bi.footer.link',
 };
-
-const FOOTER_LOGOS = process.env.FOOTER_ADDITIONAL_LOGOS;
 
 const FooterSocial = ({ intl }) => (
   <div className="footer-social d-flex mt-2">
@@ -42,8 +40,12 @@ FooterSocial.propTypes = {
 };
 
 const AdditionalLogosSection = () => {
+  const FOOTER_LOGOS = getConfig().FOOTER_ADDITIONAL_LOGOS || process.env.FOOTER_ADDITIONAL_LOGOS;
   const parseFooterLogos = () => {
     try {
+      if (Array.isArray(FOOTER_LOGOS)) {
+        return FOOTER_LOGOS;
+      }
       return JSON.parse(FOOTER_LOGOS);
     } catch (e) {
       return null;
