7afeabca498bebbbb63023581fdca272e2270647
Diana Catalina Olarte <diana.olarte@edunext.co>
2024-07-02 09:11:10 +1000

    temp: dist folder for testing

---
+++ dist/components/Footer.messages.js
@@ -0,0 +1,120 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+var messages = defineMessages({
+  'footer.languageForm.select.label': {
+    id: 'footer.languageForm.select.label',
+    defaultMessage: 'Choose Language',
+    description: 'The label for the laguage select part of the language selection form.'
+  },
+  'footer.languageForm.submit.label': {
+    id: 'footer.languageForm.submit.label',
+    defaultMessage: 'Apply',
+    description: 'The label for button to submit the language selection form.'
+  },
+  'footer.logo.altText': {
+    id: 'footer.logo.altText',
+    defaultMessage: 'Powered by Open edX',
+    description: 'alt text for the footer logo.'
+  },
+  'footer.logo.ariaLabel': {
+    id: 'footer.logo.ariaLabel',
+    defaultMessage: 'edX Home',
+    description: 'aria-label for the footer logo.'
+  },
+  'footer.ariaLabel': {
+    id: 'footer.ariaLabel',
+    defaultMessage: 'Page Footer',
+    description: 'aria-label for the footer component'
+  },
+  'footer.copyright.message': {
+    id: 'footer.copyright.message',
+    defaultMessage: 'All rights reserved',
+    description: 'Message to display on the copyright section of the footer'
+  },
+  'footer.nau.title': {
+    id: 'footer.nau.title',
+    defaultMessage: 'NAU',
+    description: 'The label for the nau section of the footer.'
+  },
+  'footer.nau.about': {
+    id: 'footer.nau.about',
+    defaultMessage: 'About',
+    description: 'The label for the link to the nau about page.'
+  },
+  'footer.nau.courses': {
+    id: 'footer.nau.courses',
+    defaultMessage: 'Courses',
+    description: 'The label for the link to the nau courses page.'
+  },
+  'footer.nau.partner': {
+    id: 'footer.nau.partner',
+    defaultMessage: 'How to become a partner',
+    description: 'The label for the link to the nau partner page.'
+  },
+  'footer.nau.communication': {
+    id: 'footer.nau.communication',
+    defaultMessage: 'Communication',
+    description: 'The label for the communication section of the nau footer.'
+  },
+  'footer.nau.help': {
+    id: 'footer.nau.help',
+    defaultMessage: 'Help',
+    description: 'The label for the link to the nau help page.'
+  },
+  'footer.nau.news': {
+    id: 'footer.nau.news',
+    defaultMessage: 'News',
+    description: 'The label for the link to the nau news page.'
+  },
+  'footer.nau.legal': {
+    id: 'footer.nau.legal',
+    defaultMessage: 'Legal',
+    description: 'The label for the legal section of the nau footer.'
+  },
+  'footer.nau.mediakit': {
+    id: 'footer.nau.mediakit',
+    defaultMessage: 'Media Kit',
+    description: 'The label for the link to the nau media kit page.'
+  },
+  'footer.nau.termsconditions': {
+    id: 'footer.nau.termsconditions',
+    defaultMessage: 'Terms and Conditions',
+    description: 'The label for the link to the nau terms and conditions page.'
+  },
+  'footer.nau.privacypolicy': {
+    id: 'footer.nau.privacypolicy',
+    defaultMessage: 'Privacy Policy',
+    description: 'The label for the link to the nau privacy policy page.'
+  },
+  'footer.nau.cookies': {
+    id: 'footer.nau.cookies',
+    defaultMessage: 'Cookies Policy',
+    description: 'The label for the link to the nau cookies policy page.'
+  },
+  'footer.nau.certification': {
+    id: 'footer.nau.certification',
+    defaultMessage: 'Certification Policy',
+    description: 'The label for the link to the nau certification policy page.'
+  },
+  'footer.nau.codeofhonor': {
+    id: 'footer.nau.codeofhonor',
+    defaultMessage: 'Code of Honor',
+    description: 'The label for the link to the nau code of honor page.'
+  },
+  'footer.nau.social.facebook': {
+    id: 'footer.nau.social.facebook',
+    defaultMessage: 'Follow us on Facebook',
+    description: 'The label for the link to the nau facebook page.'
+  },
+  'footer.nau.social.linkedin': {
+    id: 'footer.nau.social.linkedin',
+    defaultMessage: 'Follow us on LinkedIn',
+    description: 'The label for the link to the nau linkedin page.'
+  },
+  'footer.nau.social.newsletter': {
+    id: 'footer.nau.social.newsletter',
+    defaultMessage: 'Subscribe to our newsletter',
+    description: 'The label for the link to the nau newsletter page.'
+  }
+});
+export default messages;
+//# sourceMappingURL=Footer.messages.js.map
