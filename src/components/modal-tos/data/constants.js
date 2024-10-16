07b18cf0c307cbe9d82a44b13b99690ae1343101
Diana Olarte <diana.olarte@edunext.co>
2024-10-16 23:42:17 +1100

    feat: create a Modal to inform the update uf terms of service

---
+++ src/components/modal-tos/data/constants.js
@@ -0,0 +1,13 @@
+const ACCEPTANCE_TOS = 'True';
+
+const CAMEL_CASE_KEYS = {
+  date_iso_8601: 'dateIso8601',
+  data_authorization: 'dataAuthorization',
+  honor_code: 'honorCode',
+  terms_of_service: 'termsOfService',
+};
+
+export {
+  ACCEPTANCE_TOS,
+  CAMEL_CASE_KEYS,
+};
