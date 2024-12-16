8797c6f39cac6cbf002098376de7ab80f3de384d
Ivo Branco <ivo.branco@fccn.pt>
2024-12-16 17:09:50 +0000

    fix(modaltos): Translate modal ToS title
    fccn/nau-technical#367

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -154,7 +154,7 @@ class SiteFooter extends React.Component {
         </section>
         <AdditionalLogosSection />
         {
-          config.MODAL_UPDATE_TERMS_OF_SERVICE && <ModalToS />
+          config.MODAL_UPDATE_TERMS_OF_SERVICE && <ModalToS intl={intl} />
         }
       </footer>
     );
