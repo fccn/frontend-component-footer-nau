dc102f94cec3f5ed45611bb3e88c0cd8dff95628
Sandro Costa <sandro.costa@fccn.pt>
2025-05-02 23:29:12 +0000

    feat: add FooterInstitutionalLogos sections
    fccn/nau-technical#494

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -63,6 +63,24 @@ const AdditionalLogosSection = () => {
   );
 };
 
+const InstitutionalLogosSection = () => {
+  const FOOTER_INSTITUTIONAL_LOGOS = getConfig().FOOTER_INSTITUTIONAL_LOGOS || process.env.FOOTER_INSTITUTIONAL_LOGOS;
+
+  const logos = parseEnvSettings(FOOTER_INSTITUTIONAL_LOGOS);
+
+  if (!logos) { return null; }
+
+  return (
+    <section className="footer-copyright__institutional-logos">
+      {logos.map(logo => (
+        <a href={logo.url} target="_blank" rel="noopener noreferrer">
+          <img src={logo.src} alt={logo.alt} />
+        </a>
+      ))}
+    </section>
+  )
+};
+
 const FooterCopyrightSection = ({ intl }) => (
   <div className="footer-copyright__text">
     &copy; {new Date().getFullYear()} - FCT|FCCN {intl.formatMessage(messages['footer.copyright.message'])}
@@ -152,8 +170,10 @@ class SiteFooter extends React.Component {
           </div>
         </section>
         <section className="footer-copyright">
-          <div className="footer-copyright__before" />
-          <FooterCopyrightSection intl={intl} />
+          <div className="container-xl py-3 px-4">
+            <InstitutionalLogosSection />
+            <FooterCopyrightSection intl={intl} />
+          </div>
         </section>
         {
           config.MODAL_UPDATE_TERMS_OF_SERVICE && <ModalToS intl={intl} />
